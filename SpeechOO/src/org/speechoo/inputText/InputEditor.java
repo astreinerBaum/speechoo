2cb378b89ca7a510942a39f2c01118af3fb02d4b
hugoleonardoeng07 <hugoleonardoeng07@gmail.com>
2011-11-01 15:08:31 +0000

    Inserção de formatação de texto(Negrito, Posição de parágrafo, etc)
    É possível formatar o texto em negrito, itálico e sublinhado, além de escolher se o texto ficará centralizado, alinhado à esquerda ou direita ou às duas margens.
    
    As opções precisam ser escolhidas antes da dicção do texto, pois ainda não foi implementada a seleção de texto.

---
+++ SpeechOO/src/org/speechoo/inputText/InputEditor.java
@@ -0,0 +1,200 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.speechoo.inputText;
+
+import com.sun.star.awt.FontUnderline;
+import com.sun.star.awt.FontWeight;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.style.ParagraphAdjust;
+import com.sun.star.text.XTextCursor;
+import com.sun.star.uno.UnoRuntime;
+import java.beans.PropertyVetoException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 10080000701
+ */
+public class InputEditor {
+
+    public static void setBold(XTextCursor xCursor) {
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("CharWeight", new Float(FontWeight.BOLD));
+            
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputSentence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+
+    public static void setItalic(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("CharPosture", com.sun.star.awt.FontSlant.ITALIC);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputSentence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setUnderline(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("CharUnderline", FontUnderline.SINGLE);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputSentence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setFontSize(XTextCursor xCursor, float size){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("CharHeight", new Float (size));
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputSentence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setParaPosCenter(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("ParaAdjust", ParagraphAdjust.CENTER);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setParaPosRight(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("ParaAdjust", ParagraphAdjust.RIGHT);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setParaPosLeft(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("ParaAdjust", ParagraphAdjust.LEFT);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setParaPosStretch(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("ParaAdjust", ParagraphAdjust.STRETCH);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void setParaPosBlock(XTextCursor xCursor){
+        XPropertySet xCursorProps = (XPropertySet) UnoRuntime.queryInterface(
+                XPropertySet.class, xCursor);
+        try {
+
+            xCursorProps.setPropertyValue("ParaAdjust", ParagraphAdjust.BLOCK);
+
+        } catch (UnknownPropertyException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (com.sun.star.beans.PropertyVetoException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (WrappedTargetException ex) {
+            Logger.getLogger(InputEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
--- SpeechOO/src/org/speechoo/inputText/InputSentence.java
+++ SpeechOO/src/org/speechoo/inputText/InputSentence.java
@@ -2,14 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.speechoo.inputText;
 
 /**
  *
  * @author Hugo Santos
  */
-
+import com.sun.star.text.XParagraphCursor;
+import com.sun.star.text.XSentenceCursor;
 import com.sun.star.text.XText;
 import com.sun.star.text.XTextCursor;
 import com.sun.star.text.XTextDocument;
@@ -29,8 +29,32 @@ public class InputSentence {
         XText xText = xDoc.getText();
         XTextCursor xCursor = xText.createTextCursor();
         xCursor.gotoEnd(false);
+        InputEditor.setBold(xCursor);
+        InputEditor.setItalic(xCursor);
+        InputEditor.setFontSize(xCursor, 22);
+        InputEditor.setUnderline(xCursor);
+//        InputEditor.setParaPosCenter(xCursor);
+//        InputEditor.setParaPosLeft(xCursor);
+//        InputEditor.setParaPosRight(xCursor);
+        InputEditor.setParaPosBlock(xCursor);
+
+        XSentenceCursor xSC = (XSentenceCursor) UnoRuntime.queryInterface(
+                XSentenceCursor.class, xCursor);
+
+        XParagraphCursor xPC= (XParagraphCursor) UnoRuntime.queryInterface(
+                XParagraphCursor.class, xCursor);
+
+        if (!xSC.gotoPreviousSentence(true)) {
+            System.out.println("Não voltou sentença");
+        }
+
         xText.insertString(xCursor, sentence, true);
+//
+//        if (!xPC.gotoEndOfParagraph(false)) {
+//            System.out.println("Não foi pro final parágrafo");
+//        }
         xCursor.gotoEnd(false);
+        
         //xText.insertControlCharacter(xCursor, ControlCharacter.PARAGRAPH_BREAK, false);
 
 
