fa28dda2fed7e3649a0fa80a665096db2e726752
WeltonMax007 <WeltonMax007@b10b443c-a00a-baaf-2711-357553768cb6>
2012-01-24 13:54:37 +0000

    Added functions to improve the formatting of the text. Added functions for navigation. The button "Alt Gr" does the switching between dictation and grammar. Added a window to show the current status and the commands spoken.

--- SpeechOO/src/org/speechoo/recognized/FreeDictationListener.java
+++ SpeechOO/src/org/speechoo/recognized/FreeDictationListener.java
@@ -11,26 +11,20 @@ package org.speechoo.recognized;
  */
 
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.speech.recognition.Result;
 import javax.speech.recognition.ResultAdapter;
 import javax.speech.recognition.ResultEvent;
 import javax.speech.recognition.ResultToken;
-import org.speechoo.SpeechOO;
 import org.speechoo.inputText.InputSentence;
 
 public class FreeDictationListener extends ResultAdapter{
 
     InputSentence inputSentence = new InputSentence();
-    
+
     @Override
-
-public void resultAccepted(ResultEvent e) {
-
+    public void resultAccepted(ResultEvent e) {
         System.out.println("resultAccepted");
         StringBuffer returnTokens = new StringBuffer();
-        
         //StringBuffer returnTokensA = new StringBuffer();
         Result r = (Result) (e.getSource());
         ResultToken tokens[] = r.getBestTokens();
@@ -41,27 +35,6 @@ public void resultAccepted(ResultEvent e) {
                 returnTokens.append(' ');
             returnTokens.append(tokens[i].getSpokenText());
         }
-        System.out.printf("Tamanho%d:\n", +tokens.length);
-        SpeechOO.sinal = tokens.length;
-        if(SpeechOO.sinal<=2){
-            SpeechOO.gram.getActivationMode();
-            }
-        else{
-        try {
-            SpeechOO.gram.wait();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(FreeDictationListener.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        inputSentence.insertNewSentence(returnTokens.toString());
-        }
-        
-            
-            inputSentence.insertNewSentence(returnTokens.toString());
-        
-        
-       
-        
-
         /*
         for (int i = 0; i < tokensA.length; i++) {
             if (i > 0)
@@ -69,9 +42,9 @@ public void resultAccepted(ResultEvent e) {
             returnTokensA.append(tokensA[i].getSpokenText());
         }
          */
+        inputSentence.insertNewSentence(returnTokens.toString());
         //teste...
         //inputSentence.insertNewSentence(returnTokensA.toString());
-        
     }
 
     @Override
