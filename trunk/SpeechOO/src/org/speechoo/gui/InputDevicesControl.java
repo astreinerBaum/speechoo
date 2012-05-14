b48f2d65fa970fbe9827fa98874cf8f210067085
WeltonMax007@gmail.com <WeltonMax007@gmail.com@b10b443c-a00a-baaf-2711-357553768cb6>
2012-05-14 12:03:20 +0000

    The InputDevicesControl.java is a code that simulate keys of the keyboard and actions of a mouse, and the SpeechooTrayIcon.java is part of SpeechOO GUI.

---
+++ trunk/SpeechOO/src/org/speechoo/gui/InputDevicesControl.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.speechoo.gui;
+
+/**
+ *
+ * @author Welton Araújo
+ */
+
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+public class InputDevicesControl {
+protected static KeyEvent a;
+
+
+public static void rightClick() throws Exception {
+        Robot robot = new Robot();
+       
+
+        }
+ public static void keyDown() throws Exception{
+     Robot robot = new Robot();
+     robot.keyPress(KeyEvent.VK_DOWN);
+    }
+ public static void leftClick() throws Exception {
+        Robot robot = new Robot();
+        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_MASK);
+        }
+public static void keyEnter() throws Exception{
+     Robot robot = new Robot();
+     robot.keyPress(KeyEvent.VK_ENTER);
+     robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+}
