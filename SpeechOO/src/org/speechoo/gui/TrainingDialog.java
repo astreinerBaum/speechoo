ef01a5a63c98288fd6891bb65143cc2961693eec
hugoleonardoeng07 <hugoleonardoeng07@gmail.com>
2012-06-14 14:23:12 +0000

    Implementation for adapt acoustic model for user

--- SpeechOO/src/org/speechoo/gui/TrainingDialog.java
+++ SpeechOO/src/org/speechoo/gui/TrainingDialog.java
@@ -9,33 +9,46 @@ import com.sun.star.awt.PushButtonType;
 import com.sun.star.awt.XActionListener;
 import com.sun.star.awt.XButton;
 import com.sun.star.awt.XControl;
+import com.sun.star.awt.XFixedText;
+import com.sun.star.awt.XTextComponent;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.beans.UnknownPropertyException;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.deployment.PackageInformationProvider;
 import com.sun.star.deployment.XPackageInformationProvider;
 import com.sun.star.lang.EventObject;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.uno.Exception;
 import com.sun.star.uno.UnoRuntime;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.speechoo.SpeechOO;
-import org.speechoo.util.CaptureAudio;
+import org.speechoo.util.Capture;
+import ufpa.asr.frontend.Adaptador;
+import ufpa.asr.frontend.AdaptationProgress;
 
 /**
  *
  * @author Hugo Santos
  */
-public class TrainingDialog {
+public class TrainingDialog extends Thread {
 
     private String texts[];
-    private static int train = 0;
+    private static int train = -1;
     private XPropertySet xBackButtonPropertySet, xRecordButtonPropertySet,
             xStopButtonPropertySet, xNextButtonPropertySet;
     private XButton xbBack = null, xbRecord, xbStop, xbNext;
-    private CaptureAudio ca = new CaptureAudio();
-    private String recordsPath = System.getProperty("user.home") 
+    private XFixedText xFixedText;
+    private XTextComponent xTextComponent;
+    private Capture capture = new Capture();
+    private String recordsPath = System.getProperty("user.home")
             + File.separator + "coruja_jlapsapi" + File.separator + "records" + File.separator;
     private File wav;
+    private Dialog speakerAdaptationWindow;
+    private static AdaptationProgress adaptationProgress;
+    protected boolean closeWindow = false;
 
     public TrainingDialog() {
         XPackageInformationProvider xPackageInformationProvider =
@@ -46,54 +59,97 @@ public class TrainingDialog {
         File trainingTexts = new File(oxtLocation + "/dialogs/trainingtexts.xml");
         ReadFromXMLFile rfxf = new ReadFromXMLFile(trainingTexts);
         texts = rfxf.getTextsByLanguage("pt");
+        try {
+            speakerAdaptationWindow = new Dialog(SpeechOO.m_xContext, 500, 500,
+                    180, 250, "SpeechOO Records", "marralo");
+            speakerAdaptationWindow.insertFixedText(10, 10, 280,
+                    "1 - Cheque o volume do microfone para que as gravações não fiquem"
+                    + " muito baixas.\n"
+                    + "2 - Evite capturar ruídos durante a gravação.\n"
+                    + "3 - Ao concluir a gravação, o processo demorará até 10 minutos.");
+        } catch (Exception ex) {
+            Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     public void train() {
         try {
-            final Dialog dialog1 = new Dialog(SpeechOO.m_xContext, 500, 500,
-                    100, 250, "SpeechOO Records", "marralo");
-            dialog1.insertTextField(10, 10, 230, 60, texts[0]);
-
-//##############################################################################
-            Object buttonPropertiesBack = dialog1.insertButton(50, 80, 30, "Voltar",
+            train = -1;
+//######### Keep buttons properties to set during runtime ######################
+            Object buttonPropertiesBack = speakerAdaptationWindow.insertButton(50, 160, 30, "Voltar",
                     PushButtonType.STANDARD_value, false, "backButton");
 
-            XControl xButtonControlBack = dialog1.getxDialogContainer().getControl("backButton");
+            XControl xButtonControlBack = speakerAdaptationWindow.getxDialogContainer().getControl("backButton");
 
-//##############################################################################
-            wav = new File(recordsPath + "train" + train + ".wav");
-            Object buttonPropertiesRecord;
-            if (wav.exists()) {
-                buttonPropertiesRecord = dialog1.insertButton(90, 80, 30, "Gravar",
-                        PushButtonType.STANDARD_value, false, "recordButton");
-            } else {
-                buttonPropertiesRecord = dialog1.insertButton(90, 80, 30, "Gravar",
-                        PushButtonType.STANDARD_value, true, "recordButton");
-            }
-            XControl xButtonControlRecord = dialog1.getxDialogContainer().getControl("recordButton");
+//########## Keep buttons properties to set during runtime #####################
+            Object buttonPropertiesRecord = buttonPropertiesRecord = speakerAdaptationWindow.insertButton(90, 160, 30, "Gravar",
+                    PushButtonType.STANDARD_value, false, "recordButton");
+            XControl xButtonControlRecord = speakerAdaptationWindow.getxDialogContainer().getControl("recordButton");
 
-//##############################################################################
-            Object buttonPropertiesStop = dialog1.insertButton(130, 80, 30, "Parar",
+//########## Keep buttons properties to set during runtime #####################
+            Object buttonPropertiesStop = speakerAdaptationWindow.insertButton(130, 160, 30, "Parar",
                     PushButtonType.STANDARD_value, false, "stopButton");
 
-            XControl xButtonControlStop = dialog1.getxDialogContainer().getControl("stopButton");
+            XControl xButtonControlStop = speakerAdaptationWindow.getxDialogContainer().getControl("stopButton");
 
-//##############################################################################
-            Object buttonPropertiesNext = dialog1.insertButton(170, 80, 30, "Próximo",
+//########## Keep buttons properties to set during runtime #####################
+            Object buttonPropertiesNext = speakerAdaptationWindow.insertButton(170, 160, 30, "Próximo",
                     PushButtonType.STANDARD_value, true, "nextButton");
 
-            XControl xButtonControlNext = dialog1.getxDialogContainer().getControl("nextButton");
+            XControl xButtonControlNext = speakerAdaptationWindow.getxDialogContainer().getControl("nextButton");
 
-//##############################################################################
-            //Keep buttons properties to set durinf runtime
-            xBackButtonPropertySet = (XPropertySet) UnoRuntime.
-                    queryInterface(XPropertySet.class, buttonPropertiesBack);
-            xRecordButtonPropertySet = (XPropertySet) UnoRuntime.
-                    queryInterface(XPropertySet.class, buttonPropertiesRecord);
-            xStopButtonPropertySet = (XPropertySet) UnoRuntime.
-                    queryInterface(XPropertySet.class, buttonPropertiesStop);
-            xNextButtonPropertySet = (XPropertySet) UnoRuntime.
-                    queryInterface(XPropertySet.class, buttonPropertiesNext);
+//########## Keep buttons properties to set during runtime #####################
+            xBackButtonPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, buttonPropertiesBack);
+            xRecordButtonPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, buttonPropertiesRecord);
+            xStopButtonPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, buttonPropertiesStop);
+            xNextButtonPropertySet = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, buttonPropertiesNext);
+
+//########## Information of the adapting status ################################
+
+            adaptationProgress = new AdaptationProgress() {
+
+                int numberOfFiles;
+                int currentFile = 0;
+
+                public void setNumberOfFiles(int numberOfFiles) {
+                    this.numberOfFiles = numberOfFiles;
+                    System.out.println("Número de arquivos: " + this.numberOfFiles);
+                }
+
+                public void currentExtractFile() {
+                    System.out.println("Analisando arquivo: " + (++currentFile) + "/" + this.numberOfFiles);
+                    xTextComponent.setText("Analisando arquivo:" + currentFile + "/" + numberOfFiles);
+                }
+
+                public void adapting() {
+                    System.out.println("Adaptando - aguarde");
+                    xTextComponent.setText("Adaptando - aguarde");
+                }
+
+                public void finished() {
+                    try {
+                        System.out.println("Adaptação concluída");
+                        xTextComponent.setText("Adaptação concluída");
+                        xNextButtonPropertySet.setPropertyValue("PushButtonType", (short) PushButtonType.OK_value);
+                        xNextButtonPropertySet.setPropertyValue("Enabled", true);
+                        closeWindow = true;
+                    } catch (UnknownPropertyException ex) {
+                        Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (PropertyVetoException ex) {
+                        Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (WrappedTargetException ex) {
+                        Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            };
+
+            xFixedText = speakerAdaptationWindow.insertFixedText(10, 45, 230, "");
+            xTextComponent = speakerAdaptationWindow.insertTextField(10, 60, 230, 90, "");
+
+//########## Control of the buttons ############################################
 
             //Put the button into the interface
             xbBack = (XButton) UnoRuntime.queryInterface(XButton.class, xButtonControlBack);
@@ -102,19 +158,31 @@ public class TrainingDialog {
 
                 public void actionPerformed(ActionEvent arg0) {
                     try {
-                        wav = new File(recordsPath + "train" + (train - 1) + ".wav");
+                        wav = new File(recordsPath + "train" + train + ".wav");
                         if (wav.exists()) {
                             xRecordButtonPropertySet.setPropertyValue("Label", "Regravar");
                         } else {
                             xRecordButtonPropertySet.setPropertyValue("Label", "Gravar");
                         }
                         if (train == 0) {
-                            System.out.println("VOLTAR train= " + train);
-                        } else if (train == 1) {
                             xBackButtonPropertySet.setPropertyValue("Enabled", false);
-                            dialog1.insertTextField(10, 10, 230, 60, texts[--train]);
+                            xRecordButtonPropertySet.setPropertyValue("Enabled", false);
+                            xFixedText.setText("");
+                            xTextComponent.setText("");
+                            train--;
+                        } else if (train == 1) {
+                            xFixedText.setText("Treino " + (--train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
+                        } else if (train == (texts.length - 1)) {
+                            xRecordButtonPropertySet.setPropertyValue("Enabled", true);
+                            xNextButtonPropertySet.setPropertyValue("PushButtonType", (short) PushButtonType.STANDARD_value);
+                            xNextButtonPropertySet.setPropertyValue("Label", "Próximo");
+                            xNextButtonPropertySet.setPropertyValue("Enabled", true);
+                            xFixedText.setText("Treino " + (--train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
                         } else {
-                            dialog1.insertTextField(10, 10, 230, 60, texts[--train]);
+                            xFixedText.setText("Treino " + (--train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
                         }
                     } catch (Exception ex) {
                         Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
@@ -139,9 +207,10 @@ public class TrainingDialog {
                         xRecordButtonPropertySet.setPropertyValue("Enabled", false);
                         xStopButtonPropertySet.setPropertyValue("Enabled", true);
                         xNextButtonPropertySet.setPropertyValue("Enabled", false);
-
+                        System.out.println("gravando: " + recordsPath + "train" + (train + 1) + ".wav");
                         wav = new File(recordsPath + "train" + (train + 1) + ".wav");
-                        ca.startRecording(wav);
+                        //ca.startRecording(wav);
+                        capture.start(wav);
 
                     } catch (Exception ex) {
                         Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
@@ -171,8 +240,10 @@ public class TrainingDialog {
                         xRecordButtonPropertySet.setPropertyValue("Label", "Regravar");
                         xStopButtonPropertySet.setPropertyValue("Enabled", false);
                         xNextButtonPropertySet.setPropertyValue("Enabled", true);
+                        xBackButtonPropertySet.setPropertyValue("Enabled", true);
 
-                        ca.stopRecording();
+                        //ca.stopRecording();
+                        capture.stop();
 
                     } catch (Exception ex) {
                         Logger.getLogger(TrainingDialog.class.getName()).log(Level.SEVERE, null, ex);
@@ -192,19 +263,43 @@ public class TrainingDialog {
 
                 public void actionPerformed(ActionEvent arg0) {
                     try {
-                        wav = new File(recordsPath + "train" + (train + 1) + ".wav");
+                        xRecordButtonPropertySet.setPropertyValue("Enabled", true);
+                        wav = new File(recordsPath + "train" + (train + 2) + ".wav");
                         if (wav.exists()) {
                             xRecordButtonPropertySet.setPropertyValue("Label", "Regravar");
                         } else {
                             xRecordButtonPropertySet.setPropertyValue("Label", "Gravar");
                         }
-                        if (train == 0) {
+                        if (train == -1) {
                             xBackButtonPropertySet.setPropertyValue("Enabled", true);
-                            dialog1.insertTextField(10, 10, 230, 60, texts[++train]);
+                            xFixedText.setText("Treino " + (++train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
+                        } else if (train == (texts.length - 2)) {
+                            xFixedText.setText("Treino " + (++train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
+                        } else if (train == (texts.length - 1)) {
+                            xRecordButtonPropertySet.setPropertyValue("Enabled", false);
+                            xNextButtonPropertySet.setPropertyValue("Label", "Adaptar");
+                            xNextButtonPropertySet.setPropertyValue("PushButtonType", (short) PushButtonType.OK_value);
+                            xFixedText.setText("Adaptação");
+                            xTextComponent.setText("");
+                            train++;
                         } else if (train == texts.length) {
-                            System.out.println("PROXIMO train: " + train + " último treino");
+                            if (!closeWindow) {
+                                Thread thread = new TrainingDialog();
+                                thread.start();
+                                xRecordButtonPropertySet.setPropertyValue("Enabled", false);
+                                xNextButtonPropertySet.setPropertyValue("Enabled", false);
+                                xNextButtonPropertySet.setPropertyValue("Label", "Concluir");                              
+                            } else {
+                                System.out.println("Deve fechar");
+                            }
+                        } else if (train == -1) {
+                            xFixedText.setText("Treino " + (++train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
                         } else {
-                            dialog1.insertTextField(10, 10, 230, 60, texts[++train]);
+                            xFixedText.setText("Treino " + (++train + 1) + "/" + texts.length);
+                            xTextComponent.setText(texts[train]);
                         }
 
                     } catch (Exception ex) {
@@ -218,11 +313,17 @@ public class TrainingDialog {
                 }
             });
 
-            dialog1.execute();
-            dialog1.dispose();
+            speakerAdaptationWindow.execute();
+            speakerAdaptationWindow.dispose();
 
         } catch (com.sun.star.uno.Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    @Override
+    public void run() {
+        Adaptador adapter = new Adaptador(adaptationProgress);
+        adapter.startAdaptation();
     }
 }
---
+++ SpeechOO/src/org/speechoo/util/Capture.java
@@ -0,0 +1,188 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.speechoo.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
+/**
+ *
+ * @author 10080000701
+ */
+public class Capture implements Runnable {
+
+    TargetDataLine line;
+    Thread thread;
+    AudioInputStream audioInputStream;
+    double duration;
+    File file;
+
+    public void start(File file) {
+        this.file = file;
+
+        thread = new Thread(this);
+        thread.setName("Capture");
+        thread.start();
+    }
+
+    public void stop() {
+        thread = null;
+
+    }
+
+    private void shutdown(String message) {
+        if (thread != null) {
+            thread = null;
+            System.err.println(message);
+        }
+    }
+
+    public void run() {
+
+        duration = 0;
+        audioInputStream = null;
+
+        // define the required attributes for our line,
+        // and make sure a compatible line is supported.
+
+        if (file != null) {
+            createAudioInputStream(file);
+        }
+
+        AudioFormat.Encoding encoding = AudioFormat.Encoding.PCM_SIGNED;
+        float rate = 16000;
+        int sampleSize = 16;
+        int channels = 1;
+        boolean bigEndian = false;
+
+        AudioFormat format = new AudioFormat(encoding, rate, sampleSize, channels, (sampleSize / 8) * channels, rate, bigEndian);
+
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class,
+                format);
+
+        if (!AudioSystem.isLineSupported(info)) {
+            shutdown("Line matching " + info + " not supported.");
+            return;
+        }
+
+        // get and open the target data line for capture.
+
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format, line.getBufferSize());
+        } catch (LineUnavailableException ex) {
+            shutdown("Unable to open the line: " + ex);
+            ex.printStackTrace();
+            return;
+        } catch (SecurityException ex) {
+            shutdown(ex.toString());
+            ex.printStackTrace();
+            return;
+        } catch (Exception ex) {
+            shutdown(ex.toString());
+            ex.printStackTrace();
+            return;
+        }
+        
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int frameSizeInBytes = format.getFrameSize();
+        int bufferLengthInFrames = line.getBufferSize() / 8;
+        int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
+        byte[] data = new byte[bufferLengthInBytes];
+        int numBytesRead;
+
+        line.start();
+
+        while (thread != null) {
+            if ((numBytesRead = line.read(data, 0, bufferLengthInBytes)) == -1) {
+                break;
+            }
+            out.write(data, 0, numBytesRead);
+        }
+
+        // we reached the end of the stream.  stop and close the line.
+        line.stop();
+        line.close();
+        line = null;
+
+        // stop and close the output stream
+        try {
+            out.flush();
+            out.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // load bytes into the audio input stream to save on file
+
+        byte audioBytes[] = out.toByteArray();
+        ByteArrayInputStream bais = new ByteArrayInputStream(audioBytes);
+        audioInputStream = new AudioInputStream(bais, format, audioBytes.length / frameSizeInBytes);
+
+        long milliseconds = (long) ((audioInputStream.getFrameLength() * 1000) / format.getFrameRate());
+        duration = milliseconds / 1000.0;
+
+        saveToFile();
+
+        try {
+            audioInputStream.reset();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+    }
+
+    private void createAudioInputStream(File file) {
+        if (file != null && file.isFile()) {
+            try {
+                this.file = file;
+                audioInputStream = AudioSystem.getAudioInputStream(file);
+                long milliseconds = (long) ((audioInputStream.getFrameLength() * 1000) / audioInputStream.getFormat().getFrameRate());
+                duration = milliseconds / 1000.0;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Audio file required.");
+        }
+    }
+
+    private void saveToFile() {
+
+        AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+
+        if (audioInputStream == null) {
+            System.out.println("No loaded audio to save");
+            return;
+        } else if (file != null) {
+            createAudioInputStream(file);
+        }
+
+        // reset to the beginnning of the captured data
+        try {
+            audioInputStream.reset();
+        } catch (Exception e) {
+            System.out.println("Unable to reset stream " + e);
+            return;
+        }
+
+        try {
+            if (AudioSystem.write(audioInputStream, fileType, file) == -1) {
+                throw new IOException("Problems writing to file");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
