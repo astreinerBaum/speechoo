7486be4f3f8c16c26575ea82345f01ae666d5509
WeltonMax007@gmail.com <WeltonMax007@gmail.com@b10b443c-a00a-baaf-2711-357553768cb6>
2012-09-05 12:32:13 +0000

    "Updates"

--- trunk/SpeechOO/src/org/speechoo/util/Dispatch.java
+++ trunk/SpeechOO/src/org/speechoo/util/Dispatch.java
@@ -1,8 +1,7 @@
 package org.speechoo.util;
 
 // used interfaces
-import com.sun.star.awt.XWindow;
-import com.sun.star.frame.FeatureStateEvent;
+import com.sun.star.frame.FrameActionEvent;
 import com.sun.star.frame.XDesktop;
 import com.sun.star.lang.EventObject;
 import com.sun.star.lang.XMultiComponentFactory;
@@ -10,10 +9,7 @@ import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
 import org.speechoo.SpeechOO;
 import com.sun.star.frame.XDispatchProvider;
-import com.sun.star.frame.XDispatchProviderInterception;
-import com.sun.star.frame.XDispatchProviderInterceptor;
-import com.sun.star.frame.XFrame;
-import com.sun.star.frame.XStatusListener;
+import com.sun.star.frame.XFrameActionListener;
 import com.sun.star.util.XURLTransformer;
 
 public class Dispatch {
@@ -48,18 +44,7 @@ public class Dispatch {
         if (xDispatch != null) {
             xDispatch.dispatch(aURL[0], lParams);
         }
-     com.sun.star.frame.XFrames a = SpeechOO.m_xFrame.getCreator().getFrames();
-     com.sun.star.frame.XFrame[] quadros =  a.queryFrames(com.sun.star.frame.FrameSearchFlag.ALL);
-     System.out.println(quadros[0].getCreator());
-     System.out.println(SpeechOO.m_xFrame.getCreator());
-     //quadros[1].getContainerWindow().setVisible(false);
-     m_desktop = SpeechOO.m_xContext.getServiceManager().createInstanceWithContext(
-                    "com.sun.star.frame.Desktop", SpeechOO.m_xContext);
-     xdesktop = (XDesktop) UnoRuntime.queryInterface(XDesktop.class, m_desktop);
-     //System.out.println(xdesktop.getCurrentFrame().);
-     //xChildContainer.append(xFrame) ;
-    //xDispatchProvider.
-    //xWindow.setVisible(false);
+     SpeechOO.m_xFrame.setComponent(null, null);
     }
 }
     
